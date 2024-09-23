af73028c78fbc841e4f4cf45a11f7506a52f3d69
suvanshdutt <dutts1@mcmaster.ca>
2024-09-22 23:59:05 -0400

    Added framework code

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<main>
+    <h1>Hello, world!</h1>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #333;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import App from "../App.svelte";
+</script>
+
+<main>
+    <App />
+</main>
+
+<style>
+</style>
