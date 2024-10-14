3de2927af183869c78d159d38f22c1184d1e5f5e
raju1 <raju1@mcmaster.ca>
2024-10-14 18:16:10 -0400

    Updating Header.svelte

--- src/shared/Header.svelte
+++ src/shared/Header.svelte
@@ -15,4 +15,13 @@
 </main>
 
 <style>
+main {
+    background-color: white;
+    color: rgb(125, 122, 125);
+    padding: 10px 0;
+    position: fixed;
+    width: 100%;
+    z-index: 100;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+}
 </style>
