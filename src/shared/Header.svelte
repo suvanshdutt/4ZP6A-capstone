58605c6c93d363ab168a33883a073c93e48b1218
raju1 <raju1@mcmaster.ca>
2024-10-14 17:50:25 -0400

    Updating Header.svelte

--- src/shared/Header.svelte
+++ src/shared/Header.svelte
@@ -7,6 +7,8 @@
     <div class="logo">AI For Chest X-ray Read</div>
     <ul class="nav-links">
     <li><a href="#">Home</a></li>
+    <li><a href="#">Login</a></li>
+    <li><a href="#">Contact</a></li>
     </ul>
     </nav>
     
