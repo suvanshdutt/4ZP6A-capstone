ddec81f182ece0b667ced3fbf198d6c410055fd6
Yuvraj-Sandhu <yuvrajsandhu2502@gmail.com>
2024-10-15 18:29:25 -0400

    Updated Homepage with basic design

--- src/components/homepage.svelte
+++ src/components/homepage.svelte
@@ -1,24 +1,73 @@
-<script>
+<script lang="ts">
+    import { theme } from "../stores/theme";
+    import Header from "../shared/Header.svelte";
+
+
+    // using css variables
+    $: {
+        for (const [key, val] of Object.entries($theme)) {
+            document.documentElement.style.setProperty(`--${key}`, val);
+        }
+    }
 </script>
 
+<Header />
 <main>
-    <h1>Homepage 123</h1>
-    <a href="/signup"><button>Go to Signup</button></a>
+    <div class="About">
+        <h1>About</h1>
+        <p>Our Project is based on AI chest read, Our website gives the patient a radiographed report of the chest xray provided. The report checks on the potential diseases.</p>
+        <p>Project By : Suvansh Dutt, Ujjwal Raj, Yuvraj Singh Sandhu and Suhaas Parcha </p>
+        <div class="buttons">
+            <button>Login</button>
+            <button>Sign Up</button>
+        </div>
+    </div>
 </main>
 
 <style>
+    :global(body) {
+        background-color: var(--background_color);
+        color: var(--text_color);
+        margin: 0;
+        font-family: 'Alumni Sans';
+    }
     main {
-        text-align: center;
-        padding: 2em;
+        padding-top: 250px;
+        margin-left: 200px;
+        margin-right: 200px;
+    }
+    .About {
+        margin-right: 750px;
     }
 
     h1 {
-        color: #2c3e50;
+        color: #1e1e1e;
+        font-size: 80px;
+        font-family: 'Alumni Sans';
+        font-weight: 5;
     }
-
+    p {
+        color: rgba(24,23,24,0.8);
+        font-size: 20px;
+        font-family: 'Alumni Sans';
+        font-weight: 5;
+        margin-left: 25px;
+    }
+    .buttons {
+        margin-left: 100px;
+        margin-top: 100px;
+        display: flex;
+        gap: 100px;
+    }
     button {
-        padding: 0.5em 1em;
-        font-size: 1em;
+        background-color: var(--primary_color);
+        color: #fdddd7;
+        font-size: 22px;
+        font-weight: 5;
+        border-radius: 10px;
+        border: none;
         cursor: pointer;
+        width: 120px;
+        height: 48px;
     }
 </style>
