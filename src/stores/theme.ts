8d543404f9a571f314e8507e88ab2db7ecd14c3d
suvanshdutt <dutts1@mcmaster.ca>
2024-10-18 13:09:28 -0400

    Added grey text in theme

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -1,19 +1,21 @@
 import { writable, type Writable } from "svelte/store";
 
 type Theme = {
-    text_color: string;
-    background_color: string;
-    primary_color: string;
-    secondary_color: string;
-    accent_color: string;
+  text_color: string;
+  background_color: string;
+  primary_color: string;
+  secondary_color: string;
+  accent_color: string;
+  grey_text: string;
 };
 
 export const _theme: Theme = {
-    text_color: "#000000",
-    background_color: "#fbede9",
-    primary_color: "#da3029",
-    secondary_color: "#e9766e",
-    accent_color: "#000000",
+  text_color: "#000000",
+  background_color: "#fbede9",
+  primary_color: "#da3029",
+  secondary_color: "#e9766e",
+  accent_color: "#000000",
+  grey_text: "rgba(125,122,125,100%)",
 };
 
 export const theme: Writable<Theme> = writable(_theme);
