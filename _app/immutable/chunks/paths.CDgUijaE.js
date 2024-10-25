87e1f63d55a5c867ffd6dab016b0d62d878eac9f
Yuvraj-Sandhu <yuvrajsandhu2502@gmail.com>
2024-10-24 22:31:42 -0400

    Updates

---
+++ _app/immutable/chunks/paths.CDgUijaE.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_12osse5)==null?void 0:s.base)??"/4ZP6A-capstone";var e;const t=((e=globalThis.__sveltekit_12osse5)==null?void 0:e.assets)??a;export{t as a,a as b};
