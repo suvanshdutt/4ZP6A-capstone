29d27c08695765d44965ea1bbfb6cec659dbf6fd
raju1 <raju1@mcmaster.ca>
2024-11-14 18:35:41 -0500

    Dropbox for Xray File

---
+++ src/components/Dropbox.svelte
@@ -0,0 +1,43 @@
+<script></script>
+
+<main>
+  <div id="drop-area">
+    Drop the file here
+  </div>
+  <input type="file" id="file-input" hidden>
+  <div id="preview-container"></div>
+  
+</main>
+
+<style>
+/* style.css */
+body {
+  margin: 0;
+  padding: 20px;
+}
+
+h1 {
+  float: left;
+
+}
+
+#drop-area {
+  width:  200px;
+  height: 200px;
+  margin: 40px auto;
+  text-align: center;
+  line-height: 200px;
+  border: 2px dashed #fbede9;
+  background-color: #FDDDD7;
+  float: left;
+  
+}
+
+#preview-container {
+  text-align: left;
+  float: left;
+}
+
+
+
+</style>
