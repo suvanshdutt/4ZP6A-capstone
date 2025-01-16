c877efebbc2df50923b89f2f4958f673ef3caf0d
suvanshdutt <dutts1@mcmaster.ca>
2025-01-15 19:18:59 -0500

    fixed stanford

---
+++ backend/RUN_TRAINING.cmd
@@ -0,0 +1,3 @@
+set PYTHONPATH=%PYTHONPATH%;D:\College\4ZP6A-capstone\backend;D:\College\4ZP6A-capstone
+D:\College\4ZP6A-capstone\backend\venv\Scripts\python.exe D:\College\4ZP6A-capstone\backend\train.py
+pause
